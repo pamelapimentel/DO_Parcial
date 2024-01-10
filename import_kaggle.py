c8e8ee8e46096ee804f9c0cdc4fd599fe6c98edc
pamelapimentel <pamelapimentelhuerta@gmail.com>
2024-01-09 21:46:10 -0500

    update import

--- import_kaggle.py
+++ import_kaggle.py
@@ -3,14 +3,14 @@ import zipfile
 import os
 
 
-api_token = {"username":"pamepimentel","key":"77158af729f3776d09b8f8d6ef075f19"}
+api_token = {"username":"pamepimentel","key":"8a74ecfeeb2cdcbc5a460020f87b9786"}
 
 ##conectar a kaggle con token
 with open('C:/Users/USER/.kaggle/kaggle.json', 'w') as file:
     json.dump(api_token, file)
 
 ##determinar ruta de dataset
-location = "C:/Users/USER/Downloads/pipeline/dataset"
+location = "C:/Users/USER/Documents/proyecto_parcial/dataset"
 
 ##validar que la carpeta exista
 if not os.path.exists(location):
@@ -23,10 +23,10 @@ else:
             os.rmdir(os.path.join(root, name))
 
 ##se genera descarga de dataset desde plataforma KAGGLE
-os.system("kaggle datasets download -d henryshan/starbucks -p C:/Users/USER/Downloads/pipeline/dataset")
+os.system("kaggle datasets download -d henryshan/starbucks -p C:/Users/USER/Documents/proyecto_parcial/dataset")
 
 ##vamos a carpeta con archivo.zip y lo descomprimimos
-os.chdir('C:/Users/USER/Downloads/pipeline/dataset')
+os.chdir('C:/Users/USER/Documents/proyecto_parcial/dataset')
 for file in os.listdir():
     zip_ref = zipfile.ZipFile(file, 'r')
     zip_ref.extractall()
